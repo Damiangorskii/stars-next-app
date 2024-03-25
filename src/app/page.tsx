8a2bc622c96ba899394c87719c27772d5a9c3ed4
damian <damian.gorski1@wp.pl>
2024-03-25 20:16:20 +0100

    add reading from api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
-import React from 'react';
+'use client'
+
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import Layout from '../components/Layout';
+import StarList from '../components/star/StarList';
 
 interface HomeProps {
   stars: {
@@ -14,11 +18,26 @@ interface HomeProps {
 }
 
 const Home: React.FC<HomeProps> = () => {
+  const [stars, setStars] = useState<HomeProps['stars']>([]);
+
+  useEffect(() => {
+    const fetchStars = async () => {
+      try {
+        const response = await axios.get('/api/read');
+        setStars(response.data);
+      } catch (error) {
+        console.error('Error fetching stars:', error);
+      }
+    };
+
+    fetchStars();
+  }, []);
 
   return (
     <Layout>
       <div className="container mx-auto p-4">
         <h1 className="text-2xl font-bold mb-4">List of Stars</h1>
+        <StarList stars={stars} />
       </div>
     </Layout>
   );
